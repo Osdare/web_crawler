ab45dc620c72be7a567e79766a2099ebad75a170
Osdare <71006147+Osdare@users.noreply.github.com>
2025-09-18 20:47:44 +0300

    updated main

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"web_crawler/database"
+	"web_crawler/crawler"
+)
 
 func main() {
 	fmt.Println(":)")
+
+	db := database.DataBase{}
+	db.Connect("localhost:6379", "0", "")
+
+	err := db.PushUrl("https://osu.ppy.sh/")
+	if err != nil {
+		panic(err)
+	}
+
+	crawler.CrawlJob(&db)
 }
