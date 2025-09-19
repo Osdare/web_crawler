5e3e6bdd184a7e18afc3c9c5859c9be38f38cde2
Osdare <71006147+Osdare@users.noreply.github.com>
2025-09-19 15:05:00 +0300

    added some concurrency

--- crawler/crawler.go
+++ crawler/crawler.go
@@ -2,8 +2,8 @@ package crawler
 
 import (
 	"bytes"
+	"context"
 	"fmt"
-	"golang.org/x/net/html"
 	"io"
 	"log"
 	"net/http"
@@ -13,6 +13,8 @@ import (
 	"web_crawler/parser"
 	"web_crawler/types"
 	"web_crawler/utilities"
+
+	"golang.org/x/net/html"
 )
 
 // returns html as node
@@ -39,113 +41,111 @@ func Crawl(normUrl string) (*html.Node, string, error) {
 }
 
 func CrawlJob(db *database.DataBase) {
-	//infinite loop
-	ql, err := db.UrlQueueLength()
+	fmt.Println("hello :D")
+	//get url from database
+	link, err := db.PopUrl()
 	if err != nil {
-		log.Printf("aaaaaaaaa")
+		log.Printf("could not pop url from db %v\n", err)
+		return
 	}
-	for ql > 0 {
-		fmt.Println("hello :D")
-		//get url from database
-		link, err := db.PopUrl()
+	log.Printf("Crawling: %v", link)
+
+	u, err := url.Parse(link)
+	if err != nil {
+		log.Printf("could not parse url: %v %v\n", link, err)
+		return
+	}
+
+	//check if domain exists and create a new one if not
+
+	domainExists, err := db.DomainExists(u.Host)
+	if err != nil {
+		log.Printf("couldn not check if domain: %v exists %v\n", u.Host, err)
+		return
+	}
+
+	var domain types.Domain
+	if domainExists {
+		domain, err = db.GetDomain(u.Host)
 		if err != nil {
-			log.Printf("could not pop url from db %v\n", err)
-			continue
+			log.Printf("could not get domain: %v %v\n", u.Host, err)
+			return
 		}
-		log.Printf("Crawling: %v", link)
-
-		u, err := url.Parse(link)
+	} else {
+		domain, err = handlers.GetRobotsFromDomain(u.Scheme + "://" + u.Host)
 		if err != nil {
-			log.Printf("could not parse url: %v %v\n", link, err)
-			continue
+			log.Printf("could not get new domain: %v reason: %v", u.Scheme+"://"+u.Host, err)
+			return
 		}
 
-		//check if domain exists and create a new one if not
-
-		domainExists, err := db.DomainExists(u.Host)
+		err = db.AddDomain(domain)
 		if err != nil {
-			log.Printf("couldn not check if domain: %v exists %v\n", u.Host, err)
-			continue
-		}
-
-		var domain types.Domain
-		if domainExists {
-			domain, err = db.GetDomain(u.Host)
-			if err != nil {
-				log.Printf("could not get domain: %v %v\n", u.Host, err)
-				continue
-			}
-		} else {
-			domain, err = handlers.GetRobotsFromDomain(u.Scheme + "://" + u.Host)
-			if err != nil {
-				log.Printf("could not get new domain: %v reason: %v", u.Scheme+"://"+u.Host, err)
-				continue
-			}
-
-			err = db.AddDomain(domain)
-			if err != nil {
-				log.Printf("could not add domain: %v to db. Reason: %v", u.Host, err)
-				continue
-			}
-		}
-
-		//check domain and if we are allowed to crawl else continue
-		//and put url back in the queue
-		canCrawl, reason, err := handlers.CanCrawl(link, domain)
-		if err != nil {
-			log.Printf("error from CanCrawl function %v\n", err)
-			continue
-		}
-		if !canCrawl {
-			log.Printf("not allowed to crawl url: %v in domain: %v\n", link, domain.Name)
-			if reason == handlers.ReasonCrawlDelay {
-				err = db.PushUrl(link)
-			}
-			if err != nil {
-				log.Printf("could not push url to queue. Reason: %v\n", err)
-			}
-			continue
-		}
-
-		//crawl it
-		html, content, err := Crawl(link)
-		if err != nil {
-			log.Printf("Could not crawl url: %v %v\n", link, err)
-			continue
-		}
-
-		//create new page
-		//nu := strings.Replace(link, u.Scheme+"://", "", 1)
-		page := types.Page{
-			NormUrl: link,
-			Content: content,
-		}
-		err = db.AddPage(page)
-		if err != nil {
-			log.Printf("page: %v could not be added to database %v\n", link, err)
-		}
-
-		//normalize urls and put new urls in database
-		newUrls, err := parser.UrlsFromBody(html)
-		if err != nil {
-			log.Printf("Could not parse urls from html %v\n", err)
-			continue
-		}
-
-		urlsToAdd := utilities.NormalizeUrlSlice(newUrls)
-		for _, newUrl := range urlsToAdd {
-			//log.Printf("attempting to add url: %v to queue\n", newUrl)
-			err = db.PushUrl(newUrl)
-			if err != nil {
-				log.Printf("Could not add url: %v to queue %v\n", newUrl, err)
-			}
-		}
-
-		ql, err = db.UrlQueueLength()
-		log.Printf("queue length after adding urls: %v\n",ql)
-		if err != nil {
-			log.Printf("ASDFASDF")
+			log.Printf("could not add domain: %v to db. Reason: %v", u.Host, err)
+			return
 		}
 	}
-	fmt.Println("empty :D")
+
+	//check domain and if we are allowed to crawl else return
+	//and put url back in the queue
+	canCrawl, reason, err := handlers.CanCrawl(link, domain)
+	if err != nil {
+		log.Printf("error from CanCrawl function %v\n", err)
+		return
+	}
+	if !canCrawl {
+		log.Printf("not allowed to crawl url: %v in domain: %v\n", link, domain.Name)
+		if reason == handlers.ReasonCrawlDelay {
+			err = db.PushUrl(link)
+		}
+		if err != nil {
+			log.Printf("could not push url to queue. Reason: %v\n", err)
+		}
+		return
+	}
+
+	//crawl it
+	html, content, err := Crawl(link)
+	if err != nil {
+		log.Printf("Could not crawl url: %v %v\n", link, err)
+		return
+	}
+
+	//create new page
+	//nu := strings.Replace(link, u.Scheme+"://", "", 1)
+	page := types.Page{
+		NormUrl: link,
+		Content: content,
+	}
+	err = db.AddPage(page)
+	if err != nil {
+		log.Printf("page: %v could not be added to database %v\n", link, err)
+	}
+
+	//normalize urls and put new urls in database
+	newUrls, err := parser.UrlsFromBody(html)
+	if err != nil {
+		log.Printf("Could not parse urls from html %v\n", err)
+		return
+	}
+
+	urlsToAdd := utilities.NormalizeUrlSlice(newUrls)
+	for _, newUrl := range urlsToAdd {
+		//log.Printf("attempting to add url: %v to queue\n", newUrl)
+		err = db.PushUrl(newUrl)
+		if err != nil {
+			log.Printf("Could not add url: %v to queue %v\n", newUrl, err)
+		}
+	}
+}
+
+func Start(ctx context.Context, db *database.DataBase) {
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("Crawler stopping")
+			return
+		default:
+			CrawlJob(db)
+		}
+	}
 }
--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"web_crawler/database"
+	"math/rand"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
 	"web_crawler/crawler"
+	"web_crawler/database"
 )
 
 func main() {
@@ -12,10 +19,39 @@ func main() {
 	db := database.DataBase{}
 	db.Connect("localhost:6379", "0", "")
 
+	//seed url
 	err := db.PushUrl("https://osu.ppy.sh/")
 	if err != nil {
 		panic(err)
 	}
 
-	crawler.CrawlJob(&db)
+	//CTRL+C stops the program properly
+	ctx, cancel := context.WithCancel(context.Background())
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		<-sigs
+		fmt.Println("\nStopping...")
+		cancel()
+	}()
+
+	numWorkers := 5
+	var wg sync.WaitGroup
+	wg.Add(numWorkers)
+
+	for i := range numWorkers {
+		go func(workerID int) {
+			defer wg.Done()
+
+			jitter := time.Duration(rand.Intn(3000)) * time.Millisecond
+			time.Sleep(jitter)
+
+			fmt.Printf("Worker %d started after %v\n", workerID, jitter)
+			crawler.Start(ctx, &db)
+		}(i)
+	}
+
+	wg.Wait()
+	fmt.Println("All workers have stopped.")
 }
